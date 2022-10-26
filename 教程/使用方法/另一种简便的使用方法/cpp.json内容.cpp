5e33399ab0fc031f9356d4fe7e2407c863c9e1a3
BiliYunXing <116493937+BiliYunXing@users.noreply.github.com>
2022-10-26 18:37:58 +0800

    Create cpp.json内容.cpp

---
+++ 教程/使用方法/另一种简便的使用方法/cpp.json内容.cpp
@@ -0,0 +1,20 @@
+"YunAvZ": {
+		"prefix": "Yun AvZ Template",
+		"body": [
+			"#include <avz.h>",
+			"#include <TheLastYun.h>",
+			"",
+			"using namespace AvZ;",
+			"using namespace Yun;",
+			"",
+			"YunPao p;",
+			"YunCard c;",
+			"YunMaidCheats w;",
+			"",
+			"void Script()",
+			"{",
+			"",
+			"}"
+		],
+		"description": "云心的AvZ头文件模板"
+	}
